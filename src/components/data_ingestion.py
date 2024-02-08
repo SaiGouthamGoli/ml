e11bb6881f96f4d0ab3fe3fdae048551c09536a6
SaiGouthamGoli <saigouthamgoli111@gmail.com>
2024-02-08 01:02:02 -0500

    Hyper parameter Tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -52,7 +52,7 @@ if __name__=="__main__":
         data_transformation = DataTransformation()
         train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
         model_trainer = ModelTrainer()
-        print(model_trainer.initiate_model_training(train_arr, test_arr))
+        print(model_trainer.initiate_model_trainer(train_arr, test_arr))
         
 
 
